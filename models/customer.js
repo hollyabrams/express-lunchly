1871500799e3044f24c9e442d51fc7b0d6e0c0c6
Holly <holly.d.abrams@gmail.com>
2023-01-31 17:17:04 -0500

    Updated templates to reflect fullname, added a save method for reservations

--- models/customer.js
+++ models/customer.js
@@ -53,6 +53,11 @@ class Customer {
     return new Customer(customer);
   }
 
+  /** get customer fullname. */
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
   /** get all reservations for this customer. */
 
   async getReservations() {
